d37c97e8af8c5683634f205d4d52a96023edeea3
gsjuradog <gsjuradog@unal.edu.co>
2021-04-11 22:26:40 +0200

    Vue basics v-if, v-show

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('hello, vue')
+const app = Vue.createApp({
+  //an object that represents the root component
+  //data and functions go here
+  //Also the component templete goes here
+  //the properties inside data function are accesible from the template
+  data(){
+    return {
+      title: 'title bla',
+      author: 'el escritor',
+      age: 45,
+      showBooks: true,
+    }
+  },
+  methods: {
+    changeTitle(title) {
+      this.title= title
+    },
+    toggleShowBooks() {
+      this.showBooks= !this.showBooks;
+    }
+  }
+}
+);
+
+app.mount('#app');
